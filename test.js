8abc9c711faa2323fb45a896f8c6d96cc6c2e98a
CokeMine <aptx4561@gmail.com>
2021-02-18 01:02:19 +0800

    断言

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
 const axios = require('axios');
+const assert = require('assert');
 const argv = process.argv;
-if (argv.length <= 2) throw("No File changes!");
+assert.ok(argv.length <= 2, "No File changes!");
 axios.defaults.timeout = 90000;
 for (let i = 2; i < argv.length; ++i) {
     const data = JSON.parse(fs.readFileSync(argv[i], "utf-8"));
     const type = ["tech", "host", "life", "domain", "probe"];
-    if (!data["blogName"] || !data["blogLink"] || !data["type"] || type.indexOf(data["type"]) === -1) throw("再检查一下？");
-    new URL(data["blogLink"]);
-    axios.get(data["blogLink"]);
+    assert.ok(!data["blogName"] || !data["blogLink"] || !data["type"] || type.indexOf(data["type"]) === -1, "数据有误");
+    assert.doesNotThrow(() => {
+        new URL(data["blogLink"])
+    }, "检测URL是否合法");
+    (async () => {
+        await assert.doesNotReject(axios.get(data["blogLink"]), "无法正常访问");
+    })()
 }
